5f64d9a73754bba28a09df8fb922f8a546511e81
Memory Clutter <memclutter@gmail.com>
2017-12-23 10:37:07 +0300

    Simplify layout

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.home.views import IndexView, LoginView
+from apps.home.views import IndexView, LoginView, LogoutView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,7 +1,7 @@
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.views import LoginView as BaseLoginView, LogoutView as BaseLogoutView
 
 from apps.home.decorators import anonymous_required
 
@@ -14,3 +14,8 @@ class IndexView(TemplateView):
 @method_decorator(anonymous_required, name='dispatch')
 class LoginView(BaseLoginView):
     template_name = 'home/login.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class LogoutView(BaseLogoutView):
+    next_page = '/login/'
